5717ccfd591dfebaa05cfd07ef82d1f570f95ccf
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-03-18 16:25:59 +0200

    test

---
+++ JS/lesson2/task3/index.js
@@ -0,0 +1,13 @@
+let goodNumber = 17;
+let anotherNumber = goodNumber;
+anotherNumber = 77;
+console.log(goodNumber);
+console.log(anotherNumber);
+let costumer ={
+    name: "John",
+    age: 17
+}
+let anotherCostumer = costumer;
+anotherCostumer.age = 20;
+console.log(costumer.age);
+console.log(anotherCostumer.age);
