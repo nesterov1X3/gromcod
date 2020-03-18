5d4e6fc339dc023ccbf2f2be01694ca240f1e436
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-03-18 15:48:03 +0200

    test

--- JS/lesson1/task2/index.js
+++ JS/lesson1/task2/index.js
@@ -1,8 +1,8 @@
-const UserName = 'John';
+const userName = 'John';
 const greeting = "I'm John";
 const _profession = 'Developer';
 const $hobby = 'Walking';
-const FavouriteDessert = 'cake';
+const favouriteDessert = 'cake';
 let pet_ = 'dog';
 let superNumber;
 superNumber = 17;
