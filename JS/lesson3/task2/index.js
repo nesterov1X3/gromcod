0daa4e68bdd9a91f19bc944fb76b3429b35bb2bc
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-03-19 14:50:13 +0200

    test

--- JS/lesson3/task2/index.js
+++ JS/lesson3/task2/index.js
@@ -1,5 +1,5 @@
 console.log("17 to string is " + String(17));
-console.log("-17.17 to string is " + String(17.17));
+console.log("-17.17 to string is " + String(-17.17));
 console.log("false to string is " + String(false));
 console.log("null to string is " + String(null));
 console.log("undefined to string is " + String(undefined));
