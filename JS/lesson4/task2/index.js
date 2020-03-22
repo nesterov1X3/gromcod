4f25c984ea40af27c7a6e2ee7f99b93fcf5e391e
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-03-22 13:16:05 +0200

    update

--- JS/lesson4/task2/index.js
+++ JS/lesson4/task2/index.js
@@ -1,12 +1,13 @@
-let m = 1;
-let n = 12;
+const m = 1;
+const n = 12;
 let result = 1;
 
 do{
     if(m % 2===1){
-       result *=m;
+       result *= m;
     }
     m++;
 
 }while(m <= n)
+
 console.log("Result: " + result)
---
+++ JS/lesson4/task3/index.js
@@ -0,0 +1,7 @@
+for(i = 0; i < 5; i++){
+    console.log('\n=> ' + i + ' <=\n\n')
+    for(j = 0; j < 10; j++){
+        console.log(i + 'x' + j + '=' + i * j)
+    }
+
+}
