d3af283c1b31c02b1264705b92e0460dcee20276
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-03-23 00:21:08 +0200

    test

---
+++ JS/lesson4/task8/index.js
@@ -0,0 +1,20 @@
+let max = 1000;
+let result = 0;
+let int = 0;
+let float = 0;
+for(let min = 0; min <= max; min++){
+     result += min;
+         
+}
+int = result / 1234
+float = result % 1234
+
+
+switch(int > float){
+    case 1:
+        console.log(true)
+        break;
+    case 2:
+        console.log(false)
+        break;
+}
