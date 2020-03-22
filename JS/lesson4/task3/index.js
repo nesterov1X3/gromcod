8ae4466dc25cbe3b558630fff6a54dff166ddeff
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-03-22 16:04:59 +0200

    test

--- JS/lesson4/task3/index.js
+++ JS/lesson4/task3/index.js
@@ -1,8 +1,8 @@
-for(i = 1; i <= 5; i++){
+for( let i = 1; i <= 5; i++){
     
     console.log('\n=> ' + i + ' <=\n\n')
 
-    for(j = 1; j <= 10; j++){
+    for(let j = 1; j <= 10; j++){
         console.log(i + ' x ' + j + ' = ' + i * j)
     }
 
