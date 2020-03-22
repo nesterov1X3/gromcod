3e02bf06742a07cb94e7f750fec2ca65a0b33c02
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-03-22 13:26:11 +0200

    test

--- JS/lesson4/task3/index.js
+++ JS/lesson4/task3/index.js
@@ -1,5 +1,7 @@
 for(i = 1; i <= 5; i++){
-    console.log('\n=> ' + i + ' <=\n\n')
+
+    console.log('=> ' + i + ' <=')
+
     for(j = 1; j <= 10; j++){
         console.log(i + ' x ' + j + ' = ' + i * j)
     }
