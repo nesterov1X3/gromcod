06f72f038eb9e2b7a534629c10e3f9a0f5dfda19
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-03-22 13:31:14 +0200

    tet

--- JS/lesson4/task2/index.js
+++ JS/lesson4/task2/index.js
@@ -1,13 +1,13 @@
-const m = 1;
+let m = 1;
 const n = 12;
 let result = 1;
 
 do{
-    if(m % 2===1){
+    if(m % 2 === 1){
        result *= m;
     }
     m++;
 
-}while(m <= n)
+}while(m <= n);
 
-console.log("Result: " + result)
+console.log("Result: " + result);
