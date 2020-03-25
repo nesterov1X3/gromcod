c0701ed53a2a67f95072244661678b7c1443850b
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-03-25 13:08:34 +0200

    test

--- JS/lesson5/hw1/index.js
+++ JS/lesson5/hw1/index.js
@@ -1,12 +1,11 @@
 
 function getSum(a, b){
-    
+    let result = 0;
     for( let i = a ; i <= b; i++){
-        let result = 0;
        if(i % 2 === 0){
         result += i;
        }
     }
     return result;
 }
-getSum(4, 6)
+
