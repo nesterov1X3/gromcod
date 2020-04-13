5babb335114b43cd7d2fdd69855a6246a329e709
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-13 16:59:50 +0300

    test

--- JS/lesson5/hw5/index.js
+++ JS/lesson5/hw5/index.js
@@ -1,12 +1,22 @@
-function findDivCount(a, b, n){
-    let result = 0;
-    for(let i= a; i<=b; i++){
-        if(i % n ==0){
-            return result += i;
-             
-        }else
-            return 0;
-        
+// function findDivCount(a, b, n){
+//     let result = 0;
     
-} 
-}
+//     for(let i= a; i<=b; i++){
+//         if(i % n ==0){ ;
+//            result += i;
+//            return result;
+           
+//         }else
+//            return 0;
+        
+// } 
+// }
+function findDivCount(a, b, n) {
+    let result = 0;
+    for (let i = a; i <= b; i++) {
+      if (i % n === 0) {
+        result += 1;
+      }
+    }
+    return result;
+  }
