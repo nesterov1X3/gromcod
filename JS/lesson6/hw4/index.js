eb3a2c3992089fc36b2cc136c4b98bc7033baced
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-15 01:26:54 +0300

    test

--- JS/lesson6/hw4/index.js
+++ JS/lesson6/hw4/index.js
@@ -1,9 +1,10 @@
 function increaseEvenEl(arr, delta){
+    if(!Array.isArray(arr)){
+        return null;
+    }
     let sum = [];
     for(let i = 0; i <= arr.length; i++){
-        if(!Array.isArray(arr)){
-            return null;
-        }
+        
         if(arr[i] % 2 == 0){
             sum[i] = arr[i] + delta;
         }
