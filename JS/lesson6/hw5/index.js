2e073456e1380e0a9bae3bae7d567d94ba5c7981
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-15 13:20:12 +0300

    test

--- JS/lesson6/hw5/index.js
+++ JS/lesson6/hw5/index.js
@@ -1,11 +1,11 @@
 const numbers = [2, 3, 4, 7];
-const [a, b, c, d] = numbers;
-
 function reverseArray(arr){
     if(!Array.isArray(arr)){
         return null;
     }
-    return [d, c, b, a];
+    const [a, b, c, d] = numbers;
+    const rar = [d, c, b, a]
+    return rar;
 }
-const arrSwap = reverseArray([a, b, c, d])
+const arrSwap = reverseArray(numbers)
 console.log(arrSwap)
