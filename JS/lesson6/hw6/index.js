6f859e63e38000e91c466c8ef49f4eede1adc714
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-15 12:21:54 +0300

    test

--- JS/lesson6/hw6/index.js
+++ JS/lesson6/hw6/index.js
@@ -1,11 +1,12 @@
 const numbers = [3, 5, 6, 4];
+let arrNum = numbers.slice(0)
 function cloneArr(arr){
     if(!Array.isArray(arr)){
         return null;
     }
-    let arrNum = numbers.slice(0)
+    // let arrNum = numbers.slice(0)
     // const [...rest] = numbers;
     return arrNum;
 }
-const arrCopy = cloneArr(numbers);
+const arrCopy = cloneArr(arrNum);
 console.log(arrCopy)
