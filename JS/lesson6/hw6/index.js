e71a8caca67bfc0592b4347f4ba2c713139dcd56
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-16 12:33:04 +0300

    test

--- JS/lesson6/hw6/index.js
+++ JS/lesson6/hw6/index.js
@@ -1,12 +1,10 @@
 const numbers = [3, 5, 6, 4];
-const [...rest] = numbers;
-
 function cloneArr(arr){
     if(!Array.isArray(arr)){
         return null;
     }
-    const arrNum = rest;
-    return arrNum;
+    const [...rest] = numbers;
+    return rest;
 }
 const arrCopy = cloneArr(rest);
 console.log(arrCopy)
