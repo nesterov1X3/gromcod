d4fd4fcbe7728ec701c5afb523ff74266396e7c1
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-16 15:44:02 +0300

    test

--- JS/lesson6/hw10/index.js
+++ JS/lesson6/hw10/index.js
@@ -2,11 +2,11 @@ const numbers = [2, 5, 3, 1];
 
 function getSubArray(arr, n){
     let num = [];
-    for( let a of arr) {
-        if (a < n){
+    arr.forEach(function(a, index) {
+        if (index < n){
             num.push(a)
         }
-    }
+    });
     return num;
 }
 const subArray = getSubArray(numbers, 4)
