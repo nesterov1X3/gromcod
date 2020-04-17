9471cfc575a21710d476c174b92b662e33b00015
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-17 14:01:10 +0300

    test

--- JS/lesson6/hw8/index.js
+++ JS/lesson6/hw8/index.js
@@ -1,24 +1,26 @@
 const a = [21, 13, 44, 70];
-function sortAsc(array){
-    let sorted = [];
-       while (array.length) 
-    sorted = sorted.concat(array.splice(array.indexOf(array.reduce((prev, cur) => prev < cur ? prev : cur)), 1));
-    return sorted
-}
+// function sortAsc(array){
+//     let sorted = [];
+//        while (array.length) 
+//     sorted = sorted.concat(array.splice(array.indexOf(array.reduce((prev, cur) => prev < cur ? prev : cur)), 1));
+//     return sorted
+// }
 
+// const arrSort = sortAsc(a);
+// console.log(arrSort)
+
+let sortAsc = (array) => {
+    let len = array.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (array[j] > array[j + 1]) {
+                let tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+    return array;
+};
 const arrSort = sortAsc(a);
-
 console.log(arrSort)
-// let sortAsc = (inputArr) => {
-//     let len = inputArr.length;
-//     for (let i = 0; i < len; i++) {
-//         for (let j = 0; j < len; j++) {
-//             if (inputArr[j] > inputArr[j + 1]) {
-//                 let tmp = inputArr[j];
-//                 inputArr[j] = inputArr[j + 1];
-//                 inputArr[j + 1] = tmp;
-//             }
-//         }
-//     }
-//     return inputArr;
-// };
