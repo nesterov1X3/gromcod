da5bd6c82880903e0abbd92283885fd7cdc0d6f4
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-17 16:46:48 +0300

    test

--- JS/lesson6/hw13/index.js
+++ JS/lesson6/hw13/index.js
@@ -3,6 +3,9 @@ const numbers = [1, 4, 5, 4, 2, 1];
 function uniqueCount(array) {
     let result = [];
     let unical = 0; 
+    if (!Array.isArray(array)) {
+        return null;
+      }
   for (let item of array) {
     if (!result.includes(item)) {
       result.push(item);
