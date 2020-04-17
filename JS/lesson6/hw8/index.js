73728c666c50082c259621a6ab4190ccf76fe01d
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-17 14:07:11 +0300

    test

--- JS/lesson6/hw8/index.js
+++ JS/lesson6/hw8/index.js
@@ -28,3 +28,23 @@ const a = [21, 13, 44, 70];
 };
 const arrSort = sortAsc(a);
 console.log(arrSort)
+
+
+function sortDesc(array){
+    if(!Array.isArray(array)){
+        return null;
+    }
+    let len = array.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (array[j] > array[j + 1]) {
+                let tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+    return array;
+};
+const arrSort = sortDesc(a);
+console.log(arrSort)
