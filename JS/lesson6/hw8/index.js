5197102259f20503f7614cf8a6d51fee0accfad4
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-17 13:56:27 +0300

    test

--- JS/lesson6/hw8/index.js
+++ JS/lesson6/hw8/index.js
@@ -1,24 +1,24 @@
-// const a = [21, 13, 44, 70];
-// function sortAsc(array){
-//     let sorted = [];
-//        while (array.length) 
-//     sorted = sorted.concat(array.splice(array.indexOf(array.reduce((prev, cur) => prev < cur ? prev : cur)), 1));
-//     return sorted
-// }
+const a = [21, 13, 44, 70];
+function sortAsc(array){
+    let sorted = [];
+       while (array.length) 
+    sorted = sorted.concat(array.splice(array.indexOf(array.reduce((prev, cur) => prev < cur ? prev : cur)), 1));
+    return sorted
+}
 
-// const arrSort = sortAsc(a);
+const arrSort = sortAsc(a);
 
-// console.log(arrSort)
-let bubbleSort = (inputArr) => {
-    let len = inputArr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len; j++) {
-            if (inputArr[j] > inputArr[j + 1]) {
-                let tmp = inputArr[j];
-                inputArr[j] = inputArr[j + 1];
-                inputArr[j + 1] = tmp;
-            }
-        }
-    }
-    return inputArr;
-};
+console.log(arrSort)
+// let sortAsc = (inputArr) => {
+//     let len = inputArr.length;
+//     for (let i = 0; i < len; i++) {
+//         for (let j = 0; j < len; j++) {
+//             if (inputArr[j] > inputArr[j + 1]) {
+//                 let tmp = inputArr[j];
+//                 inputArr[j] = inputArr[j + 1];
+//                 inputArr[j + 1] = tmp;
+//             }
+//         }
+//     }
+//     return inputArr;
+// };
