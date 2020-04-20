8ec464453a75a423f088f6206bf72a272216ac19
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-20 23:45:03 +0300

    test

---
+++ JS/lesson7/hw1/index.js
@@ -0,0 +1,11 @@
+const squareArray = numbers => {
+    if(!Array.isArray(numbers)){
+        return null;
+    }
+    const arrSquare = numbers.map(function(item){
+        return item * item
+    });
+    return arrSquare
+}
+const arr = [2, 5, 6];
+console.log(squareArray(arr))
