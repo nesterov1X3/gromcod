c747cf6c3ecbc7d3f429b93ee54ac70292d4fd0f
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-20 19:23:02 +0300

    test

--- JS/lesson6/hw9/index.js
+++ JS/lesson6/hw9/index.js
@@ -1,8 +1,22 @@
-const clients = ['Ann', 'John', 'User'];
-const balances = [100, 13, -13];
-
-function withdraw(clients, balances, client, amount){
-    for(let i = 0; i < clients.length; i++){
-        
-    }
-}
+function withdraw(clients, balances, client, amount) {
+    console.log(clients, balances, client, amount);
+    let accountBalance = 0;
+    let result = clients.forEach(function (itemClient, indexClient) {
+      if (itemClient === client) {
+        //   console.log(true);
+        balances.forEach(function (balancesElem, indexBalance) {
+          // console.log(indexBalance);
+          if (indexClient === indexBalance) {
+            if (balancesElem > amount) {
+              accountBalance = balancesElem - amount;
+            } else {
+              accountBalance = -1;
+            }
+          }
+        });
+      }
+    });
+    return accountBalance;
+  }
+  const result = withdraw(["Ann", "John", "User"], [1400, 87, -6], "Ann", 50);
+  console.log(result);
