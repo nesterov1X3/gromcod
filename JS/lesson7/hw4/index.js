d487ad2641bab492dd4bae1dd147fbaf8cb6f85f
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-21 01:03:21 +0300

    test

---
+++ JS/lesson7/hw4/index.js
@@ -0,0 +1,10 @@
+const cloneArr = arr => {
+    if(!Array.isArray(numbers)){
+        return null;
+    }
+
+    const ArrayNumberClone = arr.slice()
+    return ArrayNumberClone
+}
+const numbers = [4, 6, 2, 5]
+console.log(cloneArr(numbers))
