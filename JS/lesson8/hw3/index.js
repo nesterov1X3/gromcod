e0cebc48f828ea685faba161775010f5832fc0de
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-21 17:55:29 +0300

    test

---
+++ JS/lesson8/hw3/index.js
@@ -0,0 +1,16 @@
+const getKeys = obj => {
+    const keysToArr = Object.keys(obj)
+     let result = []
+     keysToArr.forEach(item => {
+        result.push(obj[item])
+    });
+    return result;
+}
+obj = {
+    name: 'Bob',
+    age: 17,
+    hair: "black",
+    hant: true,
+};
+
+console.log(getKeys(obj));
