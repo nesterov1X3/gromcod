5cf6ded192d093fdc828ee7e548fff3be11ae873
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-21 14:40:44 +0300

    test

--- JS/lesson8/hw1/index.js
+++ JS/lesson8/hw1/index.js
@@ -16,7 +16,7 @@ const transaction = {
   console.log(transaction['value'])
   
   const key = 'operation';
-  console.log([key])
+  console.log(key)
   /* выведите в консоль значение свойства, название которого находится в переменной key - используйте квадратные скобки */
   
   /* выведите в консоль значение свойства 'company' используя квадратные скобки */
