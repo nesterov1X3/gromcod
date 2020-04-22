e75dee652bca8c07e29b8e6e5af1e9aade8efd49
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-22 23:58:38 +0300

    test

--- JS/lesson8/hw7/index.js
+++ JS/lesson8/hw7/index.js
@@ -4,16 +4,17 @@ const obj1 = {
 };
 const obj2 = {
     name: 'Tom', 
-    age: 17
+    age: 174
 };
 
 function compareObjects(obj1, obj2){
     let bool = false;
-    if(obj1.keys === obj2.keys){
-        bool = true;
+    if(obj1.keys == obj2.keys){
+        return true;
     }
-    return bool;
+
+    return false;
 }
 
 const result = compareObjects(obj1, obj2)
-// console.log(result)
+console.log(result)
