f17ceaace8244f4b526535189afa8e116f59ef45
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-22 13:09:31 +0300

    test

--- JS/lesson8/hw4/index.js
+++ JS/lesson8/hw4/index.js
@@ -1,33 +1,32 @@
 const user = {
     name: 'Sam'
 }
-const userld = {
-   id: 4356234343,
-}
+
+    
 //1
-function addPropertyV1(userData, userld){
-    userData.id = userld;
+function addPropertyV1(userData, userId){
+    userData.id = userId;
     return userData
 }
-addPropertyV1(user, userld)
+addPropertyV1(user, 6234343)
 
 //2
-function addPropertyV2(userData, userld){
-    Object.assign(userData, userld)
+function addPropertyV2(userData, userId){
+    Object.assign(userData, { id: userId });
     return userData;
 }
-// console.log(addPropertyV2(user, userld ))
+addPropertyV2(user, 4356234343 )
 
 //3
-function addPropertyV3(userData, userld){
+function addPropertyV3(userData, userId){
     let obj = {};
-    Object.assign(obj, userData, userld)
+    Object.assign( obj, userData, { id: userId })
     return obj;
 }
-console.log(addPropertyV3(user, userld))
+addPropertyV3(user, 9999234343)
 
 //4
-function addPropertyV4(userData, userld){
+function addPropertyV4(userData, userId){
     
     return userData;
 }
