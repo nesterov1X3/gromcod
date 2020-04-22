0a8708c72b71aa40c990adf881c450867e484af4
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-22 11:49:43 +0300

    test

--- JS/lesson8/hw3/index.js
+++ JS/lesson8/hw3/index.js
@@ -6,7 +6,7 @@ const getKeys = obj => {
     const keysToArr = Object.keys(obj)
     //  const result = [];
      keysToArr.forEach(item => {
-        console.log([item]);
+        console.log(item);
     });
     // return result;
 }
