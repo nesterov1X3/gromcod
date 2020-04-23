f64d5edb25e043003eeb318e301981e7d9fa06a0
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-23 11:44:08 +0300

    test

---
+++ JS/lesson8/hw10/index.js
@@ -0,0 +1,9 @@
+obj = {
+    'John Doe': 19,
+    'Tom': 17, 
+    'Bob': 18,
+};
+function getAdults (obj){
+ let getObjOld = {};
+ if 
+}
--- JS/lesson8/hw8/index.js
+++ JS/lesson8/hw8/index.js
@@ -1,18 +1,36 @@
 const keys = ['name', 'address', 'age'];
-const values = ['name', 'Ukraine', 'age'];
+const values = ['Bob', 'Ukraine', '18'];
 function buildObject(keysList, valuesList){
-    const obj = {};
-    keysList.reduce((acc, curent) => {
-       return  acc + curent
-    });
+    let obj = {};
+     for(let i = 0; i <= keysList.length; i++){
+      for(let j = 0; j <= valuesList.length; j++){
+        if(i == j){
+           obj[keysList[i]] = valuesList[j];
+           obj[keysList[i+1]] = valuesList[j+1];
+           obj[keysList[i+2]] = valuesList[j+2];
+        }
+      }
+      return obj;
+     }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    //   obj = keysList.reduce((acc, curent, index) => {
+  //     //  if (index == acc) {
+  //     valuesList.reduce((acc1, curent1, index1) => {
+  //        return  acc1 + curent1
+      
+  //    });
+  //  // }
+  //  });
+  //      return obj;
+    }
 
-    valuesList.reduce((acc1, curent1) => {
-        return  acc1 + curent1
-        
-     });
-
-
-    
-}
 const result = buildObject(keys, values);
-console.log(result)
+// console.log(result)
