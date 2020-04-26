06ef8dd66185d04828c27b3446e3100da3d0973b
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-26 23:28:36 +0300

    test

--- JS/lesson9/hw1/index.js
+++ JS/lesson9/hw1/index.js
@@ -11,47 +11,54 @@ const customers = {
 }
 const newObj = {...customers}
 
-const getCustomersList = (obj) => {
-    const arrUsers = Object.keys(newObj);
-    const values = Object.values(newObj);
+// const getCustomersList = (obj) => {
+//     const arrUsers = Object.keys(newObj);
+//     const values = Object.values(newObj);
+//     values.reduce((acc, elm, index) => {
+//       return (acc = elm.id = arrUsers[index]);
+//     }, {});
+//     values.sort((a, b) => a.age - b.age);
+//     return values;
+//   };
+//   const result = getCustomersList(newObj);
+//   console.log(result);
+
+
+
+const getCustomersList = customersGet => {
+
+    const getListArr = Object.entries(customersGet);
+    const copyCustumers = getListArr.slice()
+
+    const values = Object.values(customersGet);
+    const keysCustom = Object.keys(newObj);
+
+
     values.reduce((acc, elm, index) => {
-      return (acc = elm.id = arrUsers[index]);
+      return (acc = elm.id = keysCustom[index]);
     }, {});
     values.sort((a, b) => a.age - b.age);
     return values;
   };
-  const result = getCustomersList(newObj);
-  console.log(result);
 
-// const getCustomersList = customersGet => {
+    // const newArr = [];
+    // const getSomeFind = copyCustumers.map(item => {
+    //     const getId1 = item.shift()
+    //     item[0].id = getId1
+       
 
-//     const getListArr = Object.entries(customersGet);
-//     const copyCustumers = getListArr.slice()
-
-
-//     const newArr = [];
-//     const getSomeFind = copyCustumers.map(item => {
-//         const getId1 = item.shift()
-//         item[0].id = getId1
-//         copyCustumers.sort((a, b) => a.age - b.age);
-
-        // const getListValue = Object.values(item);
-        // console.log(getListValue)
-        // return getListValue.sort((a, b) => a - b);
-    //     const result = {};
-    //     item.forEach(el => {
-    //       result[el] = el;
+    
+        
     //   });
     
-    // });
 
     //  const getId1 = copyCustumers[0].shift()
     //  const getId2 = copyCustumers[1].shift()
     //  copyCustumers[0][0].id = getId1;
     //  copyCustumers[1][0].id = getId2;
 
-//     return getListArr;
+    // return getListArr;
 
-// }
-// const result = getCustomersList(customers);
-// console.log(result);
+
+const result = getCustomersList(customers);
+console.log(result);
