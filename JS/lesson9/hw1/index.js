34a74fd9d316acb9c850b2f683015fd38ca73c87
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-27 13:55:30 +0300

    test

--- JS/lesson9/hw1/index.js
+++ JS/lesson9/hw1/index.js
@@ -10,24 +10,10 @@ const customers = {
     },
 }
 
-// const newCustom = Object.assign({}, customers)
-// let newCustom = customers;
-//  const newCustom = {...customers}
-let newCustom = JSON.parse(JSON.stringify(customers));
-
-function copy(customers) {
-    let objCopy = {}; // objCopy будет хранить копию mainObj
-    let key;
-  
-    for (key in customers) {
-      objCopy[key] = customers[key]; // копирует каждое свойство objCopy
-    }
-  }
-
 const getCustomersList = customersGet => {
     // const getListArr = Object.entries(newCustom);
-    const values = Object.values(newCustom);
-    const keysCustom = Object.keys(newCustom);
+    const values = Object.values(customersGet);
+    const keysCustom = Object.keys(customersGet);
    
     values.reduce((acc, element, index) => {
               return (acc = element.id = keysCustom[index]);
@@ -37,49 +23,5 @@ const getCustomersList = customersGet => {
 
 }
 
-
-// const getCustomersList = (obj) => {
-//     const arrUsers = Object.keys(newObj);
-//     const values = Object.values(newObj);
-//     values.reduce((acc, elm, index) => {
-//       return (acc = elm.id = arrUsers[index]);
-//     }, {});
-//     values.sort((a, b) => a.age - b.age);
-//     return values;
-//   };
-//   const result = getCustomersList(newObj);
-//   console.log(result);
-
-//     const getListArr = Object.entries(customersGet);
-//     const copyCustumers = getListArr.slice()
-
-//     const values = Object.values(customersGet);
-//     const keysCustom = Object.keys(customersGet);
-
-//     const newArr = []
-//      values.reduce((acc, element, index) => {
-//       return (acc = element.id = keysCustom[index]);
-//     }, {});
-//     values.sort((a, b) => a.age - b.age);
-//     return values;
-//   };
-
-    // const newArr = [];
-    // const getSomeFind = copyCustumers.map(item => {
-    //     const getId1 = item.shift()
-    //     item[0].id = getId1
-       
-        
-    //   });
-    
-
-    //  const getId1 = copyCustumers[0].shift()
-    //  const getId2 = copyCustumers[1].shift()
-    //  copyCustumers[0][0].id = getId1;
-    //  copyCustumers[1][0].id = getId2;
-
-    // return getListArr;
-
 const result = getCustomersList(customers);
 console.log(result);
-// console.log(customers)
---
+++ JS/lesson9/hw2/index.js
@@ -0,0 +1,19 @@
+const rooms = {
+    room1: [
+        {name: 'room1 name1'},
+        {name: 'room1 name2'},
+        {name: 'room1 name3'},
+        {name: 'room1 name4'},
+    ],
+    room2: [
+        {name: 'room2 name1'},
+    ],
+    room3: [
+        {name: 'room3 name1'},
+        {name: 'room3 name2'},
+        {name: 'room3 name3'},
+    ],
+};
+const getPeople = (objRooms => {
+    const
+}
