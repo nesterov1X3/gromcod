c4ef513388fada44664bd6d39e73b73ecf20746d
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-27 23:04:11 +0300

    test

--- JS/lesson10/task4/index.js
+++ JS/lesson10/task4/index.js
@@ -1,9 +1,15 @@
 function getMaxAbsoluteNumber(arr){
+    if(!Array.isArray(arr)){
+        return null;
+    }
+    if(arr.length == 0){
+        return null;
+    }
+
     const getModul = arr.map(item =>  Math.abs(item));
     const getMax =  Math.max(...getModul);
     return getMax
+    
 }
-
 const arr = [4, -54, 6 , -4];
 const result = getMaxAbsoluteNumber(arr)
-console.log(result)
