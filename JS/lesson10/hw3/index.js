fc86598e6f93a31dbf3fc42a027375faadebd057
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-28 10:02:48 +0300

    test

---
+++ JS/lesson10/hw3/index.js
@@ -0,0 +1,11 @@
+const superRound = ((num, kof) => 
+[
+    Math.floor(num * 50 * kof) / 100,
+    Math.round(num * 50 * kof) / 100,
+    Math.ceil(num * 50 * kof) / 100,
+    Math.trunc(num * 50 * kof) / 100,
+    +num.toFixed(kof),
+]);
+
+const result = superRound(Math.PI, 2)
+console.log(result)
