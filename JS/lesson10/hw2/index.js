ce735b0b349e5592007d9dc8158c7df353042cdf
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-28 15:27:58 +0300

    test

--- JS/lesson10/hw2/index.js
+++ JS/lesson10/hw2/index.js
@@ -1,10 +1,27 @@
-function getRandomNumbers(len, start, end){
-    let arr = new Array(len);
-    arr[0] = start;
-    arr[arr.length - 1] = end;
-    const randomNum = arr.map(item => Math.random(item));
-    return randomNum
+function getRandomNumbers(len, min, max){
+    let arr = [];
+    // arr[0] = min;
+    // arr[arr.length - 1] = max;
+
+    for( let i= min; i < max; i++){
+        arr.push(random(min, max))
+     }
+     
+     function random(){
+        let rand = min - 0.5 + Math.random() * (max - min +1)
+        rand = Math.round(rand)     
+        return rand;
+    }
+
+    return arr
+    // const fullNum = arr.map(item => Number.parseInt(item));
+    // const randomNum = arr.map(item => 
+    //     Number.parseInt(Math.random(item)));
+     
+    // return randomNum
     
 }
 const result = getRandomNumbers(4, 6, 15)
 console.log(result)
+
+
