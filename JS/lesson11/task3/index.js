d012688406ac629d3deda8583cc42ca134405104
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-29 18:02:12 +0300

    test

---
+++ JS/lesson11/task3/index.js
@@ -0,0 +1,19 @@
+const splitText = (text, len = 10) => {
+    if(typeof text !== 'string'){
+        return null;
+    }
+    const strArr = [];
+    let startPosition = 0;
+    while(true) {
+        let chunk = text.substr(startPosition, len);
+        if(chunk.length === 0){
+            break;
+        }
+        strArr.push(chunk[0].toUpperCase() + chunk.slice(1))
+        startPosition += len
+        
+    }
+    return strArr.join('\n');
+}
+const result = splitText('dsqccccasefwea', 4)
+// console.log(result)
