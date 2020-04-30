4459960d769dd064f7b44c9b9671fbb6949ad264
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-30 17:32:13 +0300

    test

--- JS/lesson11/hw1/index.js
+++ JS/lesson11/hw1/index.js
@@ -1,21 +1,38 @@
-const splitString = (text, len = 10) => {
-    if(typeof text !== 'string'){
+// const splitString = (text, len = 10) => {
+//     if(typeof text !== 'string'){
+//         return null;
+//     }
+//     const strArr = [];
+//     let startPosition = 0;
+//     while(true) {
+//         let chunk = text.substr(startPosition, len); 
+//         if(chunk.length === 0){
+//             break;
+//         } 
+//          strArr.push(chunk.split(len) && chunk.padEnd(len, '.'))
+//         startPosition += len;
+//     }
+//     return strArr.join('\n');
+// }
+// const result = splitString('some message with the text', 12)
+// console.log(result)
+
+const splitString = (str, len = 10) => {
+    if (typeof str !== 'string') {
         return null;
     }
-    const strArr = [];
-    let startPosition = 0;
-    while(true) {
-        let chunk = text.substr(startPosition, len); 
-        if(chunk.length === 0){
+    let start = 0;
+    const result = [];
+    while (true) {
+        let chunk = str.substr(start, len);
+        if (chunk.length === 0) {
             break;
-        } 
-         strArr.push(chunk.split(len) && chunk.padEnd(len, '.'))
-        startPosition += len;
-
+        }
+        if (chunk.length < len) {
+            chunk = chunk.concat('.'.repeat(len - chunk.length));
+        }
+        result.push(chunk);  
+        start += len; 
     }
-    
-    return strArr.join('\n');
-}
-const result = splitString('some message with the text', 12)
-console.log(result)
-
+    return result;
+};
