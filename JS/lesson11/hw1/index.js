97f1e62e299cd7ec29e7c475c60e7590b6ad8cf6
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-04-30 12:04:40 +0300

    test

--- JS/lesson11/hw1/index.js
+++ JS/lesson11/hw1/index.js
@@ -10,11 +10,11 @@ const splitText = (text, len = 10) => {
             break;
         } 
         startPosition += len;
-        strArr.push(chunk[0].toUpperCase() + chunk.slice(1) + chunk.padEnd(len, '.'))
+        strArr.push(chunk[0].toUpperCase() + chunk.slice(1) && chunk.padEnd(len, '.'))
 
     }
     
     return strArr.join('\n');
 }
-const result = splitText('dsqchgfgcsefwea', 4)
+const result = splitText('dsqchgfefefefgcsefwea', 4)
 // console.log(result)
