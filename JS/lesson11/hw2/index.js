7e035fe647670404a3f65df2f154a5d281288028
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-01 14:19:11 +0300

    test

---
+++ JS/lesson11/hw2/index.js
@@ -0,0 +1,12 @@
+function countOccurrences(str1, str2){
+     if(str2 == ''){
+        return null
+     }
+
+    return (str1.split(str2).length - 1)
+    
+}
+const result = countOccurrences('i write some text for test', '');
+// console.log(result)
+
+
