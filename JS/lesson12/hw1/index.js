d0f3faba26e0212aea73cfe82fceda6b2d2f71ea
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-04 12:07:44 +0300

    test

--- JS/lesson12/hw1/index.js
+++ JS/lesson12/hw1/index.js
@@ -5,5 +5,5 @@ function reverseString(str){
     return str.split("").reverse().join("");
 
 }
-const result = reverseString(12345678);
+const result = reverseString("12345678");
 // console.log(result)
