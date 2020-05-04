1aa8622145491fffddb299bad50612d5f9688c9a
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-04 17:01:55 +0300

    test

--- JS/lesson12/hw2/index.js
+++ JS/lesson12/hw2/index.js
@@ -1,25 +1,48 @@
 //input: arr;
 //output: arr
-const transactionList = ['1.954', '16.44 ', 7, '1']
-function cleanTransactionList(arr){
+
+const cleanTransactionsList = (arr) => {
+    const numbers = arr.filter((el) => Number(el));
+    const deleteSpace = numbers.map((item) => Number.parseFloat(item));
+    const plusZuro = deleteSpace.map((el) => `${"$".concat(el)}`);
+    const res = plusZuro.map((el) => {
+      if (el.length <= 3) {
+        return `${el}.00`;
+      }
+      return `${el}0`;
+    });
+    return res;
+  };
+  const result = cleanTransactionsList(["  1.9 ", "16.4", 17, " 1 dollar "]);
+
+// const transactionList = ['1.954', '16.44 ', 7, '1er']
+// function cleanTransactionList(arr){
 
     
-    const makeTwoNum = arr.map(item => {
-        return Number(item).toFixed(2)
-     });
+//     const makeTwoNum = arr.map(item => {
+//         return Number(item)
+//      });
+   
+//     const numbers = makeTwoNum.filter((el) => Number(el));
+   
 
-     const numToString = makeTwoNum.map(item =>{
-        return  '$' + item 
-     });
+//      const numToString = numbers.map(item =>{
+//         return  '$' + item 
+//      });
     
-     const cleanProbel = numToString.map(item => {
-        return  item.trim() 
-     });
+//      const cleanProbel = numToString.map(item => {
+//         return  item.trim() 
+//      });
 
-     
+//      const cleanLetters = cleanProbel.filter (item => {
+//           return Number.isInteger(item)
+//      });
 
-    return cleanProbel;
-}
+//     return cleanLetters;
+// }
 
-const result = cleanTransactionList(transactionList);
+// const result = cleanTransactionList(transactionList);
 // console.log(result)
+
+
+// .toFixed(2)
