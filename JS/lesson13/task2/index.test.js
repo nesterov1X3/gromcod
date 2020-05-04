dcfeebf173e9a3234a9a29189d95a4316e9d28e4
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-04 22:53:29 +0300

    test

--- JS/lesson13/task2/index.test.js
+++ JS/lesson13/task2/index.test.js
@@ -1,4 +1,4 @@
-import {getSquarednumbers, getOddNumbers} from './index.js'
+import {getSquarednumbers, getOddNumbers} from './calculator.js'
 
 it('should get squared numbers', () =>{
 const result = getSquarednumbers([1, 2, 3]);
