bcee4307e08bd9a2fdf2b71af2da2437ee4b3529
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-05 17:59:22 +0300

    test

--- JS/lesson14/task1/index.js
+++ JS/lesson14/task1/index.js
@@ -1,10 +1,13 @@
 const message = 'Just learn it!';
 
-function  sendMessage(name) {
+export function  sendMessage(name) {
     const sender = "Gromcode";
     console.log(`${name}, ${message} You ${sender}`);
 }
 
-function setMessage(text){
-    message = text
+export function setMessage(text){
+    message = text;
 }
+
+sendMessage('Tom');
+setMessage('Hello');
