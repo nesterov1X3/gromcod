4ed4b49b253ee5ee21c9e7982e07a46d8eb9a366
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-06 15:39:24 +0300

    test

--- JS/lesson14/task2/index.js
+++ JS/lesson14/task2/index.js
@@ -1,6 +1,6 @@
 
  const createMesseger = () => {
-    let message = 'Hi';
+    let message = 'Just learn it!';
       function  sendMessage(name) {
         const sender = 'Gromcode';
         console.log(`${name}, ${message} Your ${sender}`);
@@ -9,9 +9,13 @@
       function setMessage(text){
         message = text + '!';
     }
+    function setSender(send){
+        sender = send;
+    }
     return {
         sendMessage, 
-        setMessage
+        setMessage,
+        setSender
     };
 };
 
@@ -21,10 +25,10 @@ const messenger1 = createMesseger();
 
 messenger1.sendMessage('Bob');
 
-const messenger2 = createMesseger();
+// const messenger2 = createMesseger();
 
-messenger2.sendMessage('Tom');
+// messenger2.sendMessage('Tom');
 
-messenger2.setMessage('Hello!');
+// messenger2.setMessage('Hello!');
 
-messenger1.sendMessage('Ann');
+// messenger1.sendMessage('Ann');
