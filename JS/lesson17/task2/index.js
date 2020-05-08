297f5d81a5faba66cd535838c7605d97aad2e162
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-08 14:23:03 +0300

    test

---
+++ JS/lesson17/task2/index.js
@@ -0,0 +1,12 @@
+export const callbackPrompt = {
+    message: 'Tell me your number',
+    showPrompt(){
+        const phoneNumber = prompt(this.message);
+        console.log(phoneNumber);
+    },
+    showDeferredPrompt(ms) {
+        console.log(this.showPrompt.bind(this), ms);
+    }
+};
+
+callbackPrompt.showDeferredPrompt(1000);
