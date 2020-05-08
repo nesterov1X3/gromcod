d98298a7420f18379d09592bd3bf99f02c9ab758
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-08 14:45:28 +0300

    test

---
+++ JS/lesson17/task3/index.js
@@ -0,0 +1,15 @@
+export function defer(func, ms){
+    return function(){
+       setTimeout(() => func(...arguments), ms);
+    }
+}
+
+const user = {
+    name: 'Tom',
+    sayHi() {
+        console.log(`Hi, I'am ${this.name}!`)
+    }
+}
+
+const deferredHi = defer(user.sayHi.bind(user), 1000);
+deferredHi();
