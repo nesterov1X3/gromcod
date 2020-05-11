6aff863e104fa1147416361f0e2472c25acca28a
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-11 12:34:40 +0300

    test

--- JS/lesson17/hw4/index.js
+++ JS/lesson17/hw4/index.js
@@ -13,5 +13,5 @@ export const user = {
 // const getFullNameByUs = user.getFullName.bind(user)()
 // console.log(getFullNameByUs);
 
-// console.log(user.setFullName(user.firstName, user.lastName));
+console.log(user.setFullName(user.firstName, user.lastName));
 // console.log(user);
