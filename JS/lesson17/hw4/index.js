a72d6e5e6a6192e12105cc47b50e3138626f91c3
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-11 12:32:37 +0300

    test

--- JS/lesson17/hw4/index.js
+++ JS/lesson17/hw4/index.js
@@ -3,9 +3,15 @@ export const user = {
   lastName: 'Doe',
   getFullName(){
     return `${this.firstName} ${this.lastName}`
+  },
+  setFullName(firstName, lastName ){
+      return `${this.firstName = "dad"} ${this.lastName = "sad"}`
   }
 }
 
 
 const getFullNameByUs = user.getFullName.bind(user)()
 console.log(getFullNameByUs);
+
+console.log(user.setFullName(user.firstName, user.lastName));
+console.log(user);
