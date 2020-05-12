cfc8336b6e77d3fc6e8beaa004d4bb8e65a93783
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-12 12:43:52 +0300

    test

---
+++ JS/lesson17/hw5/index.js
@@ -0,0 +1,20 @@
+const secondsTime = second => {
+  if (second < 10) {
+      return `0${second}`
+  }
+  return second;
+};
+export const timer = {
+  minsPassed: 0,
+  secondsPassed: 0,
+  _interval: null,
+  startTimer() {
+      this._interval = setInterval(() => {
+          this.secondsPassed += 5;
+          if (this.secondsPassed === 60) {
+              this.minsPassed += 1;
+              this.secondsPassed = 0;
+          }
+      }, 5000);
+  }
+}
