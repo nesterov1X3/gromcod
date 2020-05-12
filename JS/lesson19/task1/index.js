193881234dc4765902d2988f99169467faac453f
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-13 01:17:34 +0300

    test

--- JS/lesson19/task1/index.js
+++ JS/lesson19/task1/index.js
@@ -12,12 +12,13 @@ export const ship = {
     __proto__: vehicle,
     name: 'Argo',
     startMachine(){
-        console.log(`${this.name} lifting anchor up`,),
+        console.log(`${this.name} lifting anchor up`),
         this.move()
     },
     stopMachine(){
-        this.stop(),
-        console.log(`${this.name} lifting anchor down`,) 
+        
+        console.log(`${this.name} lifting anchor down`) ,
+        this.stop()
     },
     
 } 
