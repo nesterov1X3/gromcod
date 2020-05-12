044c7872e8aad2f001f1870f9e647b915e9a6617
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-12 14:38:35 +0300

    test

---
+++ JS/lesson18/task1/index.js
@@ -0,0 +1,17 @@
+export const event = {
+    guests : [
+        {name: 'John', age: 18, email: 'example@server.com'},
+        {name: 'Bob', age: 17, email: 'b@gmail.com'}
+    ],
+    message: 'Welcome to the party',
+    getInvintations(){
+        return this.guests
+            .filter(({ age }) => age >= 18)
+            .map(({name, email }) => ({
+                text: `Hi ${name}. ${this.message}`,
+                email
+            }));
+    }
+}
+
+// console.log(event.getInvintations());
