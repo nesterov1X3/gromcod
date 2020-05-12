1b96c62e35b921a1e063b540ce51e21f4b4170db
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-12 16:12:14 +0300

    test

--- JS/lesson18/task3/index.js
+++ JS/lesson18/task3/index.js
@@ -1,6 +1,8 @@
 export function sumOfSquares(){
     return [...arguments].map(item => { 
         return item * item 
+}).reduce((acc, element) => {
+    return acc + element
 })
 }
-// console.log(sumOfSquares(3, 4, 5, 32));
+console.log(sumOfSquares(3, 4, 5));
