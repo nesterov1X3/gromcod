e6d22e3efb38eb483db2ede43cc659c07e553575
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-13 14:46:00 +0300

    test

--- JS/lesson19/task2/index.js
+++ JS/lesson19/task2/index.js
@@ -1,12 +1,39 @@
-export const wallet = {
-    transactions: [1, 4, 43, 324, -4],
-    getMax() {
-        return Math.max(...this.transactions)
+export const vehicle = {
+    name: 'Kargo',
+    move() {
+        console.log(`${this.name} is moving`)
     },
-    getMin() {
-        return Math.min(...this.transactions)
-    },
-} 
-// console.log(wallet.getMax());
-// console.log(wallet.getMin());
+    stop() {
+        console.log(`${this.name} stopped`)
+    }
+}
 
+export const ship = {
+    age: 300,
+    __proto__: vehicle,
+    name: 'Argo',
+    startMachine() {
+        console.log(`${this.name} lifting anchor up`),
+            this.move()
+    },
+    stopMachine() {
+        this.stop(),
+            console.log(`${this.name} lifting anchor down`)
+
+    },
+
+}
+export function getOwnProp() {
+    for (let key in ship){
+        if(ship.hasOwnProperty(key)){
+        console.log(key);
+        }
+        
+    }
+}
+// getOwnProp()
+
+
+
+// let propShip = Object.keys(ship)
+// console.log(propShip);
