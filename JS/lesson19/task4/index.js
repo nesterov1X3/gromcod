eb49bb63d58b081e58260757e51ec598497d584f
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-13 23:52:37 +0300

    test

--- JS/lesson19/task4/index.js
+++ JS/lesson19/task4/index.js
@@ -6,12 +6,12 @@ User.prototype.sayHi = function () {
     console.log(`Hi, I am ${this.name}`);
 }
 User.prototype.requestNewPhoto = function() {
-    console.log(`New photo request was enter for ${this.name}`); 
+    console.log(`New photo request was sent for ${this.name}`); 
 }
 User.prototype.setAge = function() {
     this.age = 33;
     if(this.age >= 25){
-    console.log(`New photo request was enter for ${this.name}`); 
+      console.log(`New photo request was sent for ${this.name}`); 
     }
     if(this.age < 0){
         return false
