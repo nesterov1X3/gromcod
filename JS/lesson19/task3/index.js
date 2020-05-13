173e3d4eb078d09b992d72b4d0ad2cea773a4450
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-13 19:06:35 +0300

    test

--- JS/lesson19/task3/index.js
+++ JS/lesson19/task3/index.js
@@ -1,8 +1,8 @@
-export function user(firstName, lastName) {
+export function User(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.fullName = function(){return `${this.firstName} ${this.lastName}`}
-    this.fullName = function(value) {
+    this.getFullName = function(){return `${this.firstName} ${this.lastName}`}
+    this.setFullName = function(value) {
         const [firstName, lastName] = value.split(' ');
         this.firstName = firstName;
         this.lastName = lastName;
@@ -10,9 +10,9 @@ export function user(firstName, lastName) {
     }
 }
 
-const user1 = new user('John', 'Doe')
+const user1 = new User('John', 'Doe')
 
-console.log(user1.fullName);
+// console.log(user1.setFullName());
 
 
 
