7f72473d8610a22b0b90e6d1e9a6bdf7027106af
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-15 01:34:15 +0300

    test

---
+++ JS/lesson20/hw1/index.js
@@ -0,0 +1,27 @@
+class Order {
+    constructor(price, city, type){
+        id = Math.random();
+        this.price = price;
+        dateCreated = getTime();
+        isConfirmed = false;
+        dateConfirmed = new Date();
+        this.city = city;
+        this.type = type;
+    }
+    checkPrice(){
+        if(price > 1000){
+            return true
+        }
+        return false
+    }
+    confirmOrder(){
+        isConfirmed = true;
+        dateConfirmed = new Date();
+    }
+    isValidType(){
+        if(type == 'Buy' && type == 'Sell'){
+            return true
+        }
+        return false
+    }
+}
