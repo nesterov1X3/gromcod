e786d9a76bcc2dfc90c411ac4bfbe98d69ed0295
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-14 16:12:08 +0300

    test

--- JS/lesson20/task2/index.js
+++ JS/lesson20/task2/index.js
@@ -1,43 +1,89 @@
- const vehicle = {
-    name: 'Kargo',
+export class Vehicle {
+    constructor(name, numberOfWheels) {
+        this.name = name;
+        this.numberOfWheels = numberOfWheels;
+    }
     move() {
         console.log(`${this.name} is moving`)
-    },
+    }
     stop() {
         console.log(`${this.name} stopped`)
     }
 }
 
- const ship = {
-    age: 300,
-    __proto__: vehicle,
-    name: 'Argo',
-    startMachine() {
+export class Ship extends Vehicle {
+    constructor(name, maxspeed) {
+        super(name, 3)
+        this.maxspeed = maxspeed;
+    }
+    move() {
         console.log(`${this.name} lifting anchor up`),
-            this.move()
-    },
-    stopMachine() {
-        this.stop(),
+            super.move()
+    }
+    stop() {
+        super.stop(),
             console.log(`${this.name} lifting anchor down`)
 
-    },
-}
-
-export function getOwnProps(ship) {
- let resArrProp = []
-    for (let key in ship ){
-        if(ship.hasOwnProperty(key) && typeof ship[key] !== 'function'){
-            resArrProp.push(key)
-            
-        }
-        
     }
-    return resArrProp
 }
-// const result = getOwnProps(ship)
-// console.log(result);
+
+const vehicle1 = new Vehicle('Titanic', 3)
+const ship1 = new Ship('Titanic', 323)
+// console.log(ship1.maxspeed)
 
 
 
-// let propShip = Object.keys(ship)
-// console.log(propShip);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  const vehicle = {
+//     name: 'Kargo',
+//     move() {
+//         console.log(`${this.name} is moving`)
+//     },
+//     stop() {
+//         console.log(`${this.name} stopped`)
+//     }
+// }
+
+//  const ship = {
+//     age: 300,
+//     __proto__: vehicle,
+//     name: 'Argo',
+//     startMachine() {
+//         console.log(`${this.name} lifting anchor up`),
+//             this.move()
+//     },
+//     stopMachine() {
+//         this.stop(),
+//             console.log(`${this.name} lifting anchor down`)
+
+//     },
+// }
+
+// export function getOwnProps(ship) {
+//  let resArrProp = []
+//     for (let key in ship ){
+//         if(ship.hasOwnProperty(key) && typeof ship[key] !== 'function'){
+//             resArrProp.push(key)
+
+//         }
+
+//     }
+//     return resArrProp
+// }
