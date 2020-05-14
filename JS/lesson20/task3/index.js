51c34370e807a9d0b5a42295b5f3a857254ac161
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-14 16:28:24 +0300

    test

---
+++ JS/lesson20/task3/index.js
@@ -0,0 +1,25 @@
+export class Wallet {
+    _balance = 0;
+
+    getBalance(){
+        return this._balance
+    }
+    deposit(amount){
+        this._balance += amount;
+    }
+    withdraw(amount){
+        if(amount > this._balance){
+            console.log('Ho enouhg funds');
+            return
+        }
+        this._balance -= amount
+    }
+}
+
+// const wallet1 = new Wallet()
+// console.log(wallet1.getBalance());
+
+// wallet1.deposit(1000)
+// console.log(wallet1.getBalance());
+// wallet1.withdraw(-3333)
+// console.log(wallet1.getBalance());
