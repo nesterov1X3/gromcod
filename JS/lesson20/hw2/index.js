3b05987ae64bdd92bc6bcdc33ad506f9f607be7f
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-15 16:02:03 +0300

    test

--- JS/lesson20/hw2/index.js
+++ JS/lesson20/hw2/index.js
@@ -1,18 +1,18 @@
 export class User {
-    constructor(id, name, sessionold) {
+    constructor(id, name, sessionld) {
         this.id = id;
         this.name = name;
-        this.sessionold = sessionold;
+        this.sessionld = sessionld;
     }
 }
 
 export class extends UserRepository {
-    constructor(id, name, sessionold) {
-        super(id, name, sessionold);
+    constructor(id, name, sessionld) {
+        super(id, name, sessionld);
         this.#users = [];
     }
 
-    get #getUsersNames() {
+    get #getUserNames() {
         return this.#users.push(name);
     }
     get #getUserIds() {
