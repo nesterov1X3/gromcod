352986308ce88221a309bdf97a404a8a0c5938ec
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-18 22:49:52 +0300

    test

--- JS/lesson21/hw2/index.js
+++ JS/lesson21/hw2/index.js
@@ -2,9 +2,10 @@ export function getItemsList(){
      console.dir(document.querySelectorAll('.technology'))
      return document.querySelectorAll('.technology')
 }
-// getItemsList()
+getItemsList()
 export function getItemsArray(){
-     console.dir(document.querySelectorAll('.tool'))
-     return document.querySelectorAll('.tool')
+     let arrFromDoc = Array.from(document.querySelectorAll('.tool'))
+     console.dir(arrFromDoc)
+     return arrFromDoc
 }
-// getItemsArray()
+getItemsArray()
