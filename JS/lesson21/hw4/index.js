3994d3874c067dce6807d3d56bf88adf83db5b22
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-18 23:30:38 +0300

    test

--- JS/lesson21/hw4/index.js
+++ JS/lesson21/hw4/index.js
@@ -2,4 +2,4 @@ export function setTitle(text){
    let setSomeTitle = document.querySelector('.title')
    setSomeTitle.textContent = text;
 }
-console.log(setTitle('dom eto tophick'));
+// console.log(setTitle('dom eto tophick'));
