bc95bc9adad149cada9e24d53947dab0e5cdd13a
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-18 23:08:36 +0300

    test

---
+++ JS/lesson21/hw3/index.js
@@ -0,0 +1,20 @@
+export function getTitle(){
+     let getSomeTitle = document.querySelector('.title')
+     return getSomeTitle.textContent
+}
+// console.log(getTitle())
+export function getDescription(){
+     let getSomeDescription = document.querySelector('.about')
+     return getSomeDescription.innerText
+}
+// console.log(getDescription())
+
+export function getPlans(){
+     let getSomePlans = document.querySelector('.plans')
+     return getSomePlans.innerHTML
+}
+
+export function getGoal(){
+     let getSomeGoal = document.querySelector('.plans')
+     return getSomeGoal.outerHTML
+}
