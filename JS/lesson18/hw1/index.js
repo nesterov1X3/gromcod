3f423d98a0a6eb8672744b004c90401b7036dca3
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-18 13:49:13 +0300

    test

--- JS/lesson18/hw1/index.js
+++ JS/lesson18/hw1/index.js
@@ -1,10 +1,29 @@
-export function saveCalls(func) {
-    const calls = [];
+const timer = {
+    secondsPassed: 0,
+    minsPassed: 0,
+    timerId: null,
+    startTimer() {
+      let timerId =  setInterval(() => {
+            this.secondsPassed += 1;
+            if (this.secondsPassed === 10) {
+                this.minsPassed += 1;
+                this.secondsPassed = 0;
+            }
 
-    function withMemory(...arg) {
-        calls.push(arg);
-        return func.apply(this, arg);
-    }
-    withMemory.calls = calls;
-    return withMemory;
-};
+        }, 1000)
+    //  this.timerId = timerId;
+    },
+    getTimer() {
+      let secs  = this.secondsPassed < 10 ? `0${this.secondsPassed}` : this.secondsPassed;
+      return `${this.minsPassed}: ${secs}`
+    },
+    stopTimer() {
+      clearInterval(this.timerId);
+      this.timerId = null;
+    },
+    resetTimer() {
+        this.stopTimer();
+       this.minsPassed = 0;
+       this.secondsPassed = 0;
+    },
+}
--- JS/lesson20/hw2/index.js
+++ JS/lesson20/hw2/index.js
@@ -1,26 +1,37 @@
 
- class User {
+class User {
     constructor(id, name, sessionld) {
         this.id = id;
         this.name = name;
-        this.sessionld = sessionld;
-    }
+        this.sessionld;
+}
+        get id() {
+            return this._id;
+        }
+        get name() {
+            return this._name;
+        }
+        get sessionld() {
+            return this._sessionld;
+        }
+    
 }
 
- class extends UserRepository {
-     constructor(id, name, sessionld) {
+class UserRepository extends User{
+    constructor(id, name, sessionld) {
         super(id, name, sessionld);
         this.users = [];
     }
-    
-    
-     getUserNames() {
-        return this.#users.push(name);
+
+
+    getUserNames() {
+        return this.users.push(name);
     }
-     getUserIds() {
-        return this.#users.push(id);
+    getUserIds() {
+        return this.users.push(id);
     }
     getUserNameById() {
         return name
     }
 }
+
