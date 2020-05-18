3092c69d89b8e387f748f2be1723562bf14efa94
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-18 22:17:20 +0300

    test

---
+++ JS/lesson21/hw1/index.js
@@ -0,0 +1,7 @@
+export function getTitleElement(){
+    return console.dir(document.querySelector('title'))
+}
+// getTitleElegetInputElementment()
+export function getInputElement(){
+    return console.dir(document.querySelector('text'))
+}
