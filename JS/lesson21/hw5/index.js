c01948ddfa4d18aa15aa152bf4585678d5a1a492
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-19 00:30:48 +0300

    test

--- JS/lesson21/hw5/index.js
+++ JS/lesson21/hw5/index.js
@@ -1,6 +1,10 @@
 export function setButton(buttonText){
-   let getSomeEl = document.querySelector('body')
-  getSomeEl.innerHTML = document.createElement('button');
-  getSomeEl.textContent = buttonText
+  let pro =  document.body.innerHTML;
+   pro = document.createElement('button')
+    document.createElement('button').textContent = buttonText
+
+   //    let getSomeEl = document.querySelector('body')
+//   getSomeEl.innerHTML = document.createElement('button');
+//   getSomeEl.textContent = buttonText
 }
 // console.log(setButton('we are stupid'))
