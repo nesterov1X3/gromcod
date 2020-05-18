0ab06a25b9ea554eebd99df82b8861ddf0688553
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-18 22:44:41 +0300

    test

---
+++ JS/lesson21/hw2/index.js
@@ -0,0 +1,10 @@
+export function getItemsList(){
+     console.dir(document.querySelectorAll('.technology'))
+     return document.querySelectorAll('.technology')
+}
+// getItemsList()
+export function getItemsArray(){
+     console.dir(document.querySelectorAll('.tool'))
+     return document.querySelectorAll('.tool')
+}
+// getItemsArray()
