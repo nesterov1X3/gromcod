10696887ce66fd216e6c6809a0e9a74be84cd07e
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-20 01:33:34 +0300

    test

--- JS/lesson21/hw11/index.js
+++ JS/lesson21/hw11/index.js
@@ -3,20 +3,20 @@ export function squaredNumbers() {
      const someEl = findSomeEl.getAttribute('data-number')
      let makeArr = Array.from(findSomeEl)
      makeArr.push(someEl)
-     findSomeEl.dataset.squaredNumbers = makeArr.map(item => item * item);
+     findSomeEl.dataset.squaredNumber = makeArr.map(item => item * item);
 
 
      const findSomeEl2 = document.querySelector('ul > li:nth-child(2)')
      const someEl2 = findSomeEl2.getAttribute('data-number')
      let makeArr2 = Array.from(findSomeEl2)
      makeArr2.push(someEl2)
-     findSomeEl2.dataset.squaredNumbers = makeArr2.map(item => item * item);
+     findSomeEl2.dataset.squaredNumber = makeArr2.map(item => item * item);
 
 
      const findSomeEl3 = document.querySelector('ul > li:last-child')
      const someEl3 = findSomeEl3.getAttribute('data-number')
      let makeArr3 = Array.from(findSomeEl3)
      makeArr3.push(someEl3)
-     findSomeEl3.dataset.squaredNumbers = makeArr3.map(item => item * item);
+     findSomeEl3.dataset.squaredNumber = makeArr3.map(item => item * item);
 }
 // squaredNumbers()
