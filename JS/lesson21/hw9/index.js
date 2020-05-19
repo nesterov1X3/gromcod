27d982c0a73aad7369109c762f3818a8acf8167c
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-19 16:04:55 +0300

    test

---
+++ JS/lesson21/hw9/index.js
@@ -0,0 +1,14 @@
+export function finishForm(){
+     const findElement = document.querySelector('.login-form')
+     const makeingLogForm = document.createElement('input')
+     makeingLogForm.setAttribute('type', 'text')
+     makeingLogForm.setAttribute('name', 'login')
+     findElement.prepend(makeingLogForm)
+     
+
+     const findAnotherEl = document.querySelector('.login-form')
+     
+      
+      
+}
+// finishForm()
