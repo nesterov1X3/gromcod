856ad41f01894250e718459cf8867215b999fcdd
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-19 14:37:35 +0300

    test

--- JS/lesson21/hw7/index.js
+++ JS/lesson21/hw7/index.js
@@ -2,6 +2,6 @@ export function createButton(buttonText){
       const findBody = document.querySelector('body')
     const makeSomeButton = document.createElement('button');
      findBody.append(makeSomeButton)
-     findBody.textContent = buttonText
+     makeSomeButton.textContent = buttonText
 }
 // createButton('sast')
