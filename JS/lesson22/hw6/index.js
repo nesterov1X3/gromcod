d915dd4e29a03fede301341942df2435b7c3a27a
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-21 15:03:43 +0300

    test

--- JS/lesson22/hw6/index.js
+++ JS/lesson22/hw6/index.js
@@ -1,10 +1,11 @@
-function hadleClick(){
+function hadleClick(event){
     console.log(event.target.textContent);
     
 }
-
-const makeSomeBtn = document.querySelector('.btn');
-const makeSomeBtn1 = document.querySelector('body > button:nth-child(2)');
+const makeSomeBtn = document.querySelector('.btn')
+const makeSomeBtn1 = document.querySelector('.btn').nextElementSibling
+// const upgradeBtn = Array.from(makeSomeBtn)
+// const makeSomeBtn1 = document.querySelector('body > button:nth-child(2)');
 
 makeSomeBtn.addEventListener('click', hadleClick)
 makeSomeBtn1.addEventListener('click', hadleClick)
