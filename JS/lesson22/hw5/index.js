5d967d47c02ccd4bcd45669e982e8deeb041fe2f
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-21 14:03:28 +0300

    test

--- JS/lesson22/hw5/index.js
+++ JS/lesson22/hw5/index.js
@@ -1,7 +1,12 @@
 function makeClickOnlyOne(){
   console.log('clicked');
- 
-  
+  buttonEl.removeEventListener('click', makeClickOnlyOne)
 }
 const buttonEl = document.querySelector('.single-use-btn');
-buttonEl.addEventListener('click', makeClickOnlyOne, {once: true})
+buttonEl.addEventListener('click', makeClickOnlyOne)
+
+
+
+
+
+// buttonEl.addEventListener('click', makeClickOnlyOne, {once: true})/
