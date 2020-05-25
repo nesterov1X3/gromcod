df0f93e70947bcd3a4d2afb80f88054f4b18e8bd
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-25 23:31:49 +0300

    test

--- JS/lesson22/hw10/index.js
+++ JS/lesson22/hw10/index.js
@@ -47,7 +47,7 @@ const tasks = [
       listItemElem.innerHTML = findInput.value
       
       if (done) {
-        listItemElem.classList.add("list__item_done");
+        // listItemElem.classList.add("list__item_done");
       }
       const checkBoxElem = document.createElement("input");
       checkBoxElem.setAttribute("type", "checkbox");
