3110b2ab714e935ca386cfacecc3fc8612abc4f4
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-27 22:18:02 +0300

    test

--- JS/lesson24/task2/index.js
+++ JS/lesson24/task2/index.js
@@ -1,8 +1,9 @@
-const formatter = new Intl.DateTimeFormat('en',{
+export const formatter = new Intl.DateTimeFormat('en',{
     timeZone: 'UTC',
     hour: '2-digit',
     minute: '2-digit',
     hour12: false
 });
 const getGreenwichTime = date => formatter.format(date)
-console.log(getGreenwichTime(new Date()));
+// console.log(getGreenwichTime(new Date()));
+
