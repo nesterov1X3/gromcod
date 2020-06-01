15d21f8a0c79bfd8f9d2c116d0586d6e33dc13c1
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-01 16:24:05 +0300

    test

--- JS/lesson24/hw3/index.js
+++ JS/lesson24/hw3/index.js
@@ -1,95 +1,116 @@
 
-const listElem = document.querySelector('.list');
+
+const listElem = document.querySelector(".list");
+
 const tasks = [
-    { text: 'Buy milk', done: false, createDate: '2019-11-24T15:51:32.222Z', id: '1' },
-    { text: 'Pick up Tom from airport', done: false, createDate: '2019-11-25T15:51:32.222Z', id: '2' },
-    { text: 'Visit party', done: false, createDate: '2019-11-26T15:51:32.222Z', id: '3' },
-    { text: 'Visit doctor', done: true, createDate: '2019-11-27T15:51:32.222Z', id: '4' },
-    { text: 'Buy meat', done: true, createDate: '2019-11-28T15:51:32.222Z', id: '5' },
+  {
+    text: "Buy milk",
+    done: false,
+    createDate: new Date().toISOString(),
+    doneDate: null,
+    id: "1",
+  },
+  {
+    text: "Pick up Tom from airport",
+    done: false,
+    createDate: new Date().toISOString(),
+    doneDate: null,
+    id: "2",
+  },
+  {
+    text: "Visit party",
+    done: false,
+    createDate: new Date().toISOString(),
+    doneDate: null,
+    id: "3",
+  },
+  {
+    text: "Visit doctor",
+    done: true,
+    createDate: new Date().toISOString(),
+    doneDate: new Date().toISOString(),
+    id: "4",
+  },
+  {
+    text: "Buy meat",
+    done: true,
+    createDate: new Date().toISOString(),
+    doneDate: new Date().toISOString(),
+    id: "5",
+  },
 ];
 
 const compareTasks = (a, b) => {
-    if (a.done - b.done !== 0) {
-        return a.done - b.done;
-    };
-    if (a.done) {
-        return new Date(b.finishDate) - new Date(a.finishDate);
-    }
-    return new Date(b.createDate) - new Date(a.createDate);
+  if (a.done - b.done !== 0) {
+    return a.done - b.done;
+  }
+  if (a.done) {
+    return new Date(b.doneDate) - new Date(a.doneDate);
+  }
+  return new Date(b.createDate) - new Date(a.createDate);
 };
 
-const renderTasks = tasksList => {
-    listElem.innerHTML = '';
-    const tasksElems = tasksList
-        .slice()
-        .sort(compareTasks)
-        .map(({ text, done, id }) => {
-            const listItemElem = document.createElement('li');
-            listItemElem.classList.add('list__item');
-            const checkbox = document.createElement('input');
-            checkbox.setAttribute('type', 'checkbox');
-            checkbox.setAttribute('data-id', id);
-            // checkbox.setAttribute('data-create', createDate);
-            checkbox.checked = done;
-            checkbox.classList.add('list__item-checkbox');
-            
-            if (done) {
-                listItemElem.classList.add('list__item_done');
-            }
-            listItemElem.append(checkbox, text);
-            const compareTasks = (a, b) => {
-                if (a.done - b.done !== 0) {
-                    return a.done - b.done;
-                };
-                if (a.done) {
-                    return new Date(b.finishDate) - new Date(a.finishDate);
-                }
-                return new Date(b.createDate) - new Date(a.createDate);
-            };
-            return listItemElem;
-            
-        });
-    listElem.append(...tasksElems);
+const renderTasks = (tasksList) => {
+  listElem.innerHTML = "";
+  const tasksElems = tasksList
+    .slice()
+    .sort(compareTasks)
+    .map(({ text, done, id }) => {
+      const listItemElem = document.createElement("li");
+      listItemElem.classList.add("list__item");
+      const checkbox = document.createElement("input");
+      checkbox.setAttribute("type", "checkbox");
+      checkbox.setAttribute("data-id", id);
+      checkbox.checked = done;
+      checkbox.classList.add("list__item-checkbox");
+      if (done) {
+        listItemElem.classList.add("list__item_done");
+      }
+      listItemElem.append(checkbox, text);
+      return listItemElem;
+    });
+  listElem.append(...tasksElems);
 };
+
 renderTasks(tasks);
 
+const onToggleTask = (e) => {
+  const isCheckbox = e.target.classList.contains("list__item-checkbox");
 
-const onToggleTask = e => {
-    const isCheckbox = e.target.classList.contains('list__item-checkbox');
-    if (!isCheckbox) {
-        return;
-    }
-    const taskData = tasks.find(task => task.id === e.target.dataset.id);
-    Object.assign(taskData, { done: e.target.checked });
-    renderTasks(tasks);
+  if (!isCheckbox) return;
+  
+  const taskData = tasks.find((task) => task.id === e.target.dataset.id);
+
+  taskData.done = e.target.checked;
+  taskData.done
+    ? (taskData.doneDate = new Date().toISOString())
+    : (taskData.doneDate = null);
+
+  renderTasks(tasks);
 };
 
+const todoListElem = document.querySelector(".list");
+todoListElem.addEventListener("click", onToggleTask);
 
-
- 
-const todoListElem = document.querySelector('.list');
-todoListElem.addEventListener('click', onToggleTask);
-const createBtnElem = document.querySelector('.create-task-btn');
+const createBtnElem = document.querySelector(".create-task-btn");
 const onCreateTask = () => {
-    const taskTitleInputElem = document.querySelector('.task-input');
-    const text = taskTitleInputElem.value;
-    if (!text) {
-        return;
-    }
-    taskTitleInputElem.value = '';
-    tasks.push({
-        text,
-        done: false,
-        createDate: new Date().toISOString(),
-        id: Math.random().toString()
-    });
-    renderTasks(tasks);
-
-    
-    
+  const taskTitleInputElem = document.querySelector(".task-input");
+  const text = taskTitleInputElem.value;
+  if (!text) {
+    return;
+  }
+  taskTitleInputElem.value = "";
+  tasks.push({
+    text,
+    done: false,
+    createDate: new Date().toISOString(),
+    doneDate: null,
+    id: Math.random().toString(),
+  });
+  renderTasks(tasks);
 };
-createBtnElem.addEventListener('click', onCreateTask);
 
+createBtnElem.addEventListener("click", onCreateTask);
 
 
 
