4d55c24470e5eaa6a0b641c2be99117682917eef
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-01 11:45:06 +0300

    test

--- JS/lesson24/hw2/index.js
+++ JS/lesson24/hw2/index.js
@@ -1,23 +1,59 @@
 //algo
 //input: arr
-//output: -
+//output: obj
 //create obj, ket- mounth, value-arr
 //sort arr for day of mounth
 //must stay  only name
+
+// 1. Create function getStudentsBirthDays
+// 2. input: [{ name: 'Tom', birthDate: '01/15/2010' }, { name: 'Ben', birthDate: '01/17/2008' }, { name: 'Sam', birthDate: '03/15/2010' }]
+//    output: { Jan: ['Tom', 'Ben'], Mar: ['Sam'] }
+// 3. Detect month of birth and group
+// 4. Sort students inside the month
+// 5. Leave only names
+
 const students = [{ name: 'Tom', birthDate: '01/15/2010' },
 { name: 'Ben', birthDate: '01/17/2008' },
 { name: 'Sam', birthDate: '03/15/2010' }]
 
 export function studentsBirthDays(students) {
-     let copStud = [...students]
-    
+     let monStud = ['Jan', 'Feb', 'Mar']
+    const studentsObj = students.reduce((acc, student) => {
+     //     console.log(student);
+         const monthNumber = new Date(student.birthDate).getMonth()
+         const montName = monStud[monthNumber];
+         const oldStudents = acc[montName] ? acc[montName] : []
+         return {...acc, [montName] : oldStudents.concat(student)}
+     //     console.log(montName);
+     
+    }, {});
+    for (let month in studentsObj) {
+     const sortedStudents = studentsObj[month].sort((a, b) => {
+         return (
+             new Date(a.birthDate).getDate() -
+             new Date(b.birthDate).getDate()
+         );
+     });
+     const studentsNames = sortedStudents.map((student) => student.name);
+     studentsObj[month] = studentsNames;
+ }
+
+
+    return studentsObj
+
+
+
+
+
+
      // const studSpl = copStud.map(item => 
      //      item.birthDate.split('/'))
      // console.log(studSpl);
      
-     const studentByMounth = Object.assign({}, { 'Jan': students.
-     filter(item => item)
-     }, { 'Mar': students })
+     // const studentByMounth = Object.assign({}, { 'Jan': students.
+     // filter(item => item)
+     // }, { 'Mar': students })
      // console.log(studentByMounth)
      ;}
-// studentsBirthDays(students)
+const result =  studentsBirthDays(students)
+console.log(result);
