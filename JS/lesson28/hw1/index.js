84d81ee72cc63335547ca446edb3ddc4afa4f972
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-03 22:34:02 +0300

    test

--- JS/lesson28/hw1/index.js
+++ JS/lesson28/hw1/index.js
@@ -6,24 +6,65 @@
 //
 
 export const shmoment = initDate => {
-  let result = initDate;
+  let result = new Date(initDate);
   const calc = {
-    yaers(value){
-      result += value;
-      return this
-    },
-    add(date){
-      
-      return this;
+    add(date, value){
+       if(date == 'years'){
+        result = new Date(result.setFullYear
+          (result.getFullYear() + value)) 
+       }else if(date == 'months'){
+          result = new Date(result.setMonth(
+            result.getMonth() + value))
+       }else if(date == 'days'){
+        result = new Date(result.setDay(
+          result.getDay() + value))
+       }else if(date == 'hours'){
+        result = new Date(result.setHours(
+          result.getHours() + value))
+       }else if(date == 'minute'){
+        result = new Date(result.setMinutes(
+          result.getMinutes() + value))
+       }else if(date == 'seconds'){
+        result = new Date(result.setSeconds(
+          result.getSeconds() + value))
+       }else if(date == 'milliseconds'){
+        result = new Date(result.setMilliseconds(
+          result.getMilliseconds() + value))
+       }
+      return result;
+
     },
     subtract(date, value){
-      result -= valdateue;
-      return this
+      if(date == 'years'){
+        result = new Date(result.setFullYear
+          (result.getFullYear() - value)) 
+       }else if(date == 'months'){
+          result = new Date(result.setMonth(
+            result.getMonth() - value))
+       }else if(date == 'days'){
+        result = new Date(result.setDay(
+          result.getDay() - value))
+       }else if(date == 'hours'){
+        result = new Date(result.setHours(
+          result.getHours() - value))
+       }else if(date == 'minute'){
+        result = new Date(result.setMinutes(
+          result.getMinutes() - value))
+       }else if(date == 'seconds'){
+        result = new Date(result.setSeconds(
+          result.getSeconds() - value))
+       }else if(date == 'milliseconds'){
+        result = new Date(result.setMilliseconds(
+          result.getMilliseconds() - value))
+       }
+      return result
     },
     result(){
       return result
-    }
+    },
 
   };
   return calc;
 }
+const result = shmoment(new Date(2020, 0, 6, 43, 41, 55).add('minutes', 2).add('days', 43).result())
+// console.log(result);
