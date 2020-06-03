d4c49aba9a7645b232fe77295fa71b6049858ea0
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-03 15:26:32 +0300

    test

--- JS/lesson28/task3/index.js
+++ JS/lesson28/task3/index.js
@@ -1,11 +1,12 @@
 export const mult = a => b => a * b;
 
-export const twice = mult(4)
+export const twice = mult(2)
 
-const result = twice(2)
+// const result = twice(4)
+// console.log(result);
 
-export const triple = mult(4)
-const result1 =  triple(3)
+export const triple = mult(3)
+// const result1 =  triple(5)
 
 // console.log(result1);
 
