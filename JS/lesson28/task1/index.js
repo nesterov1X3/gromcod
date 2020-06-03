fca6f15ce6939a324560b833e3dbf7847a6add16
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-03 13:50:02 +0300

    test

--- JS/lesson28/task1/index.js
+++ JS/lesson28/task1/index.js
@@ -23,5 +23,5 @@ export const calc = initValue => {
   };
   return calculator
 }
-const result = calc(3).add(2).mult(4).div(10).subtract(5).result();
+// const result = calc(3).add(2).mult(4).div(10).subtract(5).result();
 // console.log(result);
