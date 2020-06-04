437e0796e82848f1de11fe206337869c4cdc6a23
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-04 15:45:42 +0300

    test

--- JS/lesson29/task3/index.js
+++ JS/lesson29/task3/index.js
@@ -1,10 +1,10 @@
 export const printing = () => {
     console.log(1);
-    setTimeout(function(){console.log(2); }, 0)
-    setTimeout(function(){console.log(3); }, 10)
-    setTimeout(function(){console.log(4); }, 1000)
-    setTimeout(function(){console.log(5); }, 2000)
+    setTimeout(function(){console.log(4); }, 0)
+    console.log(2);
+    setTimeout(function(){console.log(5); }, 1000)
+    console.log(3);
     
 }
 
-// printing()
+printing()
