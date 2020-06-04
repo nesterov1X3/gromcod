7b9cfe95139466544ba7214bf80ef8ab45cc8619
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-04 15:19:54 +0300

    test

---
+++ JS/lesson29/task2/index.js
@@ -0,0 +1,15 @@
+//pigner
+
+const pigner = (num, period) => {
+  let i = num;
+  console.log('Ping');
+  const interval = setInterval(() => {
+    if(--i > 0){
+      console.log('Ping');
+    }else{
+      clearInterval(interval);
+    }
+  }, period)
+}
+
+// pigner(5, 1000)
