42a34c6a4d8f9e6cdff727b4b8d85b51ffff1d64
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-09 00:23:40 +0300

    test

---
+++ JS/lesson31/hw2/index.js
@@ -0,0 +1,31 @@
+const serverResponsePromise = new Promise(resolve => {
+  const serverResponse = {
+      ok: true,
+      json() {
+          return Promise.resolve({
+              name: 'John',
+              age: 20
+          });
+      }
+  }
+  resolve(serverResponse);
+});
+
+/*
+* допишите первый обработчик, чтобы во второй попал объект пользователя
+*/
+
+serverResponsePromise
+  .then(response => {
+      /* ...code here */
+      const user = {
+        name: 'John',
+        age: 20
+      }
+      return user
+  })
+  .then(result => {
+      console.log(result); // { name: 'John', age: 20 }
+  });
+
+console.log('!!! Обратите внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!');
