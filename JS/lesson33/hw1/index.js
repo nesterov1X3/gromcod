c7bc0c8caba791e9953a1ce15c09fdd1319bd11e
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-10 16:51:13 +0300

    test

--- JS/lesson33/hw1/index.js
+++ JS/lesson33/hw1/index.js
@@ -1,7 +1,7 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
 /* getTasksList code here */
-const getTasksList = baseUrl=> {
+export const getTasksList = baseUrl=> {
    return fetch(baseUrl)
    .then(tasksList => tasksList.json())
 }
@@ -9,7 +9,7 @@ getTasksList(baseUrl)
 .then(tasksList => console.log(tasksList))
 /* getTaskById code here */
 
-const getTaskById = taskid => {
+export const getTaskById = taskid => {
     return fetch(`https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks/${taskid}`)
    .then(taskData => taskData.json())
 }
