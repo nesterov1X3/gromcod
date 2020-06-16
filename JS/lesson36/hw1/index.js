00678fce66bc3f067861903d18e746ea80dabb1b
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-16 15:50:16 +0300

    test

--- JS/lesson36/hw1/index.js
+++ JS/lesson36/hw1/index.js
@@ -3,23 +3,36 @@
 //put fetchBlogs to getUsersBlogs
 //
 
+// const fetchBlogs = USERID => {
+//     const response = fetch(`https://api.github.com/users/${USERID}`)
+//     if (response.ok) {
+//         return response.json()
+//     }
+//     throw new Error('Failed to load data')
+// }
 
-export const getUsersBlogs = async () => {
-    
-    try{
-        const reposList = fetchBlogs(blog);
-    
-    } catch(err){
-        console.log(err.message); 
+// export const getUsersBlogs = async users => {
+
+//     try {
+//         const reposList = fetchBlogs([...users].blog);
+//         const usBlogs = await Promise.all([...reposList])
+//         return usBlogs
+//     } catch (err) {
+//         console.log(err.message);
+//     }
+
+// } 
+const getUsersBlogs = async usersList => {
+    try {
+        const requests = usersList
+            .map(userId => fetch(`https://api.github.com/users/${userId}`).then(response => response.json()));
+        const usBlogs = await Promise.all(requests);
+        const users = usBlogs.map(({ blog }) => blog)
+        return users;
+    } catch (err) {
+        throw new Error('Failed to load data');
     }
-    
-}
+};
+getUsersBlogs(['facebook', 'git'])
+    .then(linksList => console.log(linksList))
 
-
-const fetchBlogs = USERID => {
-    const response = fetch(`https://api.github.com/users/${USERID}`)
-    if(response.ok){
-        return  response.json()
-      }
-      throw  new Error('Failed to load data')
-}
